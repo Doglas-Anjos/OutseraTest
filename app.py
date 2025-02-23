f538703ddf09473fafa41ef9c5306efe343bc178
Doglas Anjos <doglasquerino@gmail.com>
2025-02-23 17:21:26 -0300

    Criando app.py para ser server flask da api, já está consultando os dados no banco e trazendo as informações conforme solicitação

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
+from models import *
+from flask import Flask
+from flask_restful import Api
+from load_database import carregar_arquivo_no_banco, processa_lista_de_filmes_e_transforma_em_produtores
+from defines import path_arquivo_importacao
+
+app = Flask(__name__)
+api = Api(app)
+
+carregar_arquivo_no_banco(path_arquivo_importacao)
 
 
+@app.route('/')
+def encontra_filmes():
+    dict_processamento = processa_lista_de_filmes_e_transforma_em_produtores()
+    return dict_processamento
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
