7de6a4a0888e583b6c8e3a4e5d7b37827f9bbf06
Doglas Anjos <doglasquerino@gmail.com>
2025-02-23 22:38:11 -0300

    adicionando cache para aplicação ficar mais rápida

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from models import *
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from load_database import carregar_arquivo_no_banco, processa_lista_de_filmes_e_transforma_em_produtores
 from defines import path_arquivo_importacao
+from flask_caching import Cache
 
 app = Flask(__name__)
 api = Api(app)
+# configurando cache para ser mais rápido a resposta para usuario uma vez que a api é 'estática'
+app.config['CACHE_TYPE'] = 'simple'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 60
+cache = Cache(app)
 
 try:
     carregar_arquivo_no_banco(path_arquivo_importacao)
@@ -13,10 +18,12 @@ except Exception as e:
     print(f"Houve um problema ao inserir arquivo banco: -> \n {e}")
 
 
-@app.route('/')
+@app.route('/producers', methods=['GET'])
+@cache.cached()
 def encontra_filmes():
     dict_processamento = processa_lista_de_filmes_e_transforma_em_produtores()
-    return dict_processamento
+    return jsonify(dict_processamento), 200
+
 
 
 if __name__ == '__main__':
